58f6b8b12b00925efd45a8e5e35ed0d76e906ad2
hemilrajpura-simform <hemil.r@simformsolutions.com>
2024-06-18 10:28:35 +0530

    ui update for login

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -1,9 +1,10 @@
 "use client";
+import { Button } from "@mui/material";
 import { useSession, signIn, signOut } from "next-auth/react";
 
 export default function Component() {
   const { data: session } = useSession();
-  if (session) {
+  if (false) {
     return (
       <>
         Signed in as {session.user.email} <br />
@@ -12,9 +13,36 @@ export default function Component() {
     );
   }
   return (
-    <>
-      Not signed in <br />
-      <button onClick={() => signIn("github")}>Sign in using Github</button>
-    </>
+    <div className="flex items-center justify-center min-h-screen">
+      <div className="text-center">
+        {session ? (
+          <>
+            Signed in as {session.user?.name}
+            <br />
+            <Button
+              className="m-2.5"
+              variant="contained"
+              onClick={() => signOut()}
+            >
+              Sign out
+            </Button>
+          </>
+        ) : (
+          <>
+            <Button variant="text" color="error">
+              Not signed in
+            </Button>
+            <br />
+            <Button
+              className="m-2.5"
+              variant="contained"
+              onClick={() => signIn("github")}
+            >
+              Sign in using Github
+            </Button>
+          </>
+        )}
+      </div>
+    </div>
   );
 }
